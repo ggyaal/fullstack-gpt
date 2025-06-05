09bb3ff325dd69d2e47f567ba2d80088611cf458
jeelee <>
2025-06-05 19:11:20 +0900

    Learning Streamlit

---
+++ Home.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+st.set_page_config(
+  page_title="FullstackGPT",
+  page_icon="🤖"
+)
+
+st.title("FullstackGPT")
+st.subheader("This is Chat bot!")
+
+st.markdown(
+    """
+# Hello!
+            
+Welcome to my FullstackGPT Portfolio!
+            
+Here are the apps I made:
+            
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InvestorGPT](/InvestorGPT)
+"""
+)
